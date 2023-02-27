a94b36e022b1768bfc9e35a7ca3add5baba9778d
Pasindu Vinod <PasinduV@fortude.co>
2023-02-27 12:59:42 +0530

    Web application completely implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import * as XLSX from "xlsx";
 import axios from "axios";
@@ -7,28 +7,27 @@ function App() {
 
   const [items, setItems] = useState([]); //excel sheet data
   const [team, setTeam] = useState("");
-  const [date, setDate] = useState(""); 
+  const [date, setDate] = useState("");
   const [empList, setEmpList] = useState([]); //all selected employees
   const [isUploaded, setIsUploaded] = useState(false) //wheck wheather the file is uploaded or not
   const [sheets, setSheets] = useState([])
+  const [jsonObj, setJsonObj] = useState([])
   const [selectedTeam, setSelectedTeam] = useState()
-  const [formData, setFormData] = useState( 
+  const [formData, setFormData] = useState(
     {
       date: "",
       team: "",
       jsonObj: [
-        {
-          Employee: "32432",
-          EPF: "048589",
-          Gender: "F",
-          CallingName: "Nalani",
-          From: "4.45pm",
-          To: "7.45pm",
-          Transport: "Ekala",
-          Remarks: "Associate"
-        }
+
       ]
     })
+
+  useEffect(() => {
+     
+    setFormData(formData => ({ ...formData, date: date, team: team }))
+   
+  }, [date, team]);
+
 
   //select handler for groups
   const options = ['Team-01', 'Team-02', 'Team-03', 'Team-04', 'Team-05', 'General', 'Sample Cutting', 'Sample Warehouse', 'Quality Assurance', 'Mechanic'];
@@ -42,17 +41,18 @@ function App() {
 
   //checks the checked check boxes and unchecked check boxes. If checked, the checked data row will be added to the array "empList" 
   const UpdateEmployees = (e) => {
-      if (e.target.checked) {
-        setEmpList((oldArray) => [...oldArray, e.target.value]);
-        console.log(empList)
-      }else{
-        removeEmployee(e); //calling remove employee function
-        console.log(empList)
-      }
+    console.log(e.target.value)
+
+    if (e.target.checked) {
+      setEmpList((oldArray) => [...oldArray, e.target.value]);
+    } else {
+      removeEmployee(e); //calling remove employee function
+      console.log(empList)
     }
+  }
 
   //removes data from the emplist array when unchecked a checkbox
-  const removeEmployee = (e) =>{
+  const removeEmployee = (e) => {
     setEmpList([...empList.filter((d) => d !== e.target.value)])
   }
 
@@ -67,12 +67,12 @@ function App() {
 
         setIsUploaded(true)
         const importRange = "A9:O40";
-        const headers = ["Employee", "EPF", "Gender", "CallingName","", "From", "To", "Transport","","","","","","","Remarks"];
+        const headers = ["Employee", "EPF", "Gender", "CallingName", "", "From", "To", "Transport", "", "", "", "", "", "", "Remarks"];
         const bufferArray = e.target.result;
         const wb = XLSX.read(bufferArray, { type: "buffer" });
         setSheets(wb.SheetNames)
         const ws = wb.Sheets[wb.SheetNames[0]];
-        const data = XLSX.utils.sheet_to_json(ws, {range: importRange, header: headers});
+        const data = XLSX.utils.sheet_to_json(ws, { range: importRange, header: headers });
         resolve(data);
 
       };
@@ -83,156 +83,156 @@ function App() {
     });
 
     promise.then((d) => {
-        console.log("Item: "+ d[0])
-        setItems(d);      
+      setItems(d);
     });
   };
 
-const SubmitList = (event) => {
-  event.preventDefault();
-  // const jsonObj = { ...Object.fromEntries(empList) };
-  // console.log(jsonObj)
+  const SubmitList = async (event) => {
+    event.preventDefault();
+    console.log("step1");
+    for (let i = 0; i < empList.length; i++) {
+      var myArray = empList[i].split(',');
+      let obj = {
+        "Employee": myArray[1],
+        "EPF": myArray[3],
+        "Gender": myArray[5],
+        "CallingName": myArray[7],
+        "From": myArray[9],
+        "To": myArray[11],
+        "Transport": myArray[13],
+        "Remarks": myArray[15]
+      }
+      console.log(i, ":", empList.length);
+      console.log("step2");
+      //setJsonObj((oldArray) => [...oldArray, obj]);
+      jsonObj[i] = obj;
+      console.log(jsonObj);
 
-  // setEmpList([{"Employee":"12312", "EPF":"5432", "Gender":"F","callingName":"Sama","From":"4.45pm","To":"7.45pm", "Transport":"Ekala","Remarks":"Assosiate"}]);
-  // const formData = {
-  //   team: team,
-  //   date: date,
-  //   // EmpList: empList, 
-  //   jsonObj:[{"Employee":"12312", "EPF":"5432", "Gender":"F","callingName":"Sama","From":"4.45pm","To":"7.45pm", "Transport":"Ekala","Remarks":"Assosiate"}],
-  // };
- alert(date, team)
-  // const formData = 
-  //   {
-  //     date: date,
-  //     team: team,
-  //     jsonObj: [
-  //       {
-  //         Employee: "32432",
-  //         EPF: "048589",
-  //         Gender: "F",
-  //         CallingName: "Nalani",
-  //         From: "4.45pm",
-  //         To: "7.45pm",
-  //         Transport: "Ekala",
-  //         Remarks: "Associate"
-  //       }
-  //     ]
-  //   }
-  setFormData({...formData,date:date,team:team})
-  console.log("--------------")
-  console.log(empList)
-  console.log("------------")
+      if (i == empList.length-1) {
+        //setFormData(formData => ({ ...formData, date: date, team: team, jsonObj: jsonObj }));
+        let objform = {
+          "date": date, "team": team, "jsonObj": jsonObj
+        }
+        formData[0] = objform;
+        console.log(formData);
+        console.log("step3");
 
-  console.log(JSON.stringify(formData))
-  const dataa = JSON.stringify(formData)
-  axios.post("https://prod-45.southeastasia.logic.azure.com:443/workflows/543c2338f6bb45a88578d57562140705/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5snvi_lEWw3EjVQ5gFqrOTnJGDip2lsubI7ehzvt8FE", dataa,{headers: {
-    'Content-Type': 'application/json'}
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+        const dataa = JSON.stringify(formData)
+        if (dataa.jsonObj !== null) {
+          console.log("step4");
+          console.log(dataa);
+          await axios.post("https://prod-45.southeastasia.logic.azure.com:443/workflows/543c2338f6bb45a88578d57562140705/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5snvi_lEWw3EjVQ5gFqrOTnJGDip2lsubI7ehzvt8FE", dataa, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+            .then((response) => {
+              console.log("step5");
+              console.log(response);
+            });
+        }
+      }
+    }
+
+
+  };
 
   return (
     <div className="App">
-    
+
       <div>
         {/* Following div will only visible to users after excel sheet selected, date selected, and group selected */}
-        {console.log(formData)}
+        {/* console.log(formData) */}
         {isUploaded === true && date !== '' && team !== '' ? (
           <div>
             <form onSubmit={SubmitList}>
-            <h1>Daily OT & Transport Approval Request Form</h1>
-            <h4>Date: {date}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Team: {team}</h4>
-            <h3>පහත සඳහන් කාල සීමාව තුලදී අතිකාල සේවයේ යෙදීමට අපගේ කැමැත්ත ප්‍රකාශ කර සිටිමු. </h3>
-            <table className="table">
-              <thead>
-              <tr>
-                 <th>Employee#</th>
-                 <th>EPF#</th>
-                 <th>Gender</th>
-                 <th>Calling Name</th>
-                 <th>From</th>
-                 <th>To</th>
-                 <th>Transport</th>
-                 <th>Remarks</th>
-                 <th>Agree</th>
-              </tr>
-              </thead>
-              {/* Maps employee data that comes from Items(Excelsheet data) */}
+              <h1>Daily OT & Transport Approval Request Form</h1>
+              <h4>Date: {date}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Team: {team}</h4>
+              <h3>පහත සඳහන් කාල සීමාව තුලදී අතිකාල සේවයේ යෙදීමට අපගේ කැමැත්ත ප්‍රකාශ කර සිටිමු. </h3>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Employee#</th>
+                    <th>EPF#</th>
+                    <th>Gender</th>
+                    <th>Calling Name</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Transport</th>
+                    <th>Remarks</th>
+                    <th>Agree</th>
+                  </tr>
+                </thead>
+                {/* Maps employee data that comes from Items(Excelsheet data) */}
                 {items.map((d) => {
-                  
-                    return(
-                      <tr key={d.Employee}>
-                        <td>{d.Employee}</td>
-                        <td>{d.EPF}</td>
-                        <td>{d.Gender}</td>
-                        <td>{d.CallingName}</td>
-                        <td>{d.From}</td>
-                        <td>{d.To}</td>
-                        <td>{d.Transport}</td>
-                        <td>{d.Remarks}</td>
-                        <td><input type="checkbox"
-                              name="employees"
-                              // value={[d.Employee, d.EPF, d.Gender, d.CallingName, d.From, d.To, d.Transport, d.Remarks]}
-                              value={[...Object.entries(d)]}
-                              // value={{"Employee" : d.Employee, "EPF" : d.EPF, "Gender" : d.Gender, "Calling Name" : d.CallingName, "From" : d.From, "To" : d.To, "Transport" : d.Transport, "Remark" : d.Remark}}
-                              onChange = {(e) => {UpdateEmployees(e)}}
-                            />
-                        </td>{console.log(d)}
-                      </tr>
-                    )
+
+                  return (
+                    <tr key={d.Employee}>
+                      <td>{d.Employee}</td>
+                      <td>{d.EPF}</td>
+                      <td>{d.Gender}</td>
+                      <td>{d.CallingName}</td>
+                      <td>{d.From}</td>
+                      <td>{d.To}</td>
+                      <td>{d.Transport}</td>
+                      <td>{d.Remarks}</td>
+                      <td><input type="checkbox"
+                        name="employees"
+                        // value={{"Employee" : d.Employee, "EPF" : d.EPF}}
+                        value={[...Object.entries(d)]}
+                        onChange={(e) => { UpdateEmployees(e) }}
+                      />
+                      </td>
+                    </tr>
+                  )
                 }
                 )}
-                  <tbody>
+                <tbody>
                 </tbody>
               </table>
               <button id="SubmitBtn" type="submit"> Submit</button>
             </form>
-          </div>  
+          </div>
         ) : (
           // Following part will visible to users initially
           <div>
-          <h1>Daily OT & Transport Approval Request Form</h1>
-          <h3>Upload Excel File</h3>
-          <div>
-            {/* upload file */}
-            <input
-              className="inputFile"
-              type="file"
-              onChange={(e) => {
-                const file = e.target.files[0];
-                readExcel(file);
-            }}
-            />
-  
-            {/* select date */}
-            <label>Enter Date: </label>
-            <input 
-             type="date"
-             onChange={(e) => setDate(e.target.value)}
-            />
+            <h1>Daily OT & Transport Approval Request Form</h1>
+            <h3>Upload Excel File</h3>
+            <div>
+              {/* upload file */}
+              <input
+                className="inputFile"
+                type="file"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  readExcel(file);
+                }}
+              />
 
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            {/* select group */}
-            <label>Select Group : </label>
+              {/* select date */}
+              <label>Enter Date: </label>
+              <input
+                type="date"
+                onChange={(e) => setDate(e.target.value)}
+              />
 
-            <select onChange={onOptionChangeHandler}>
-             <option>Please choose one option</option>
-              {options.map((option, index) => {
-              return <option key={index} >
-              {option}
-             </option>
-              })}
-            </select>
- 
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {/* select group */}
+              <label>Select Group : </label>
+
+              <select onChange={onOptionChangeHandler}>
+                <option>Please choose one option</option>
+                {options.map((option, index) => {
+                  return <option key={index} >
+                    {option}
+                  </option>
+                })}
+              </select>
+
+            </div>
           </div>
-        </div>
         )}
-      </div>    
+      </div>
     </div>
   );
 }
